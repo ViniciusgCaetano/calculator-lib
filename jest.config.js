1e922160a013dddbc8dda2d60608f57b67edb665
ViniciusgCaetano <viny6888@gmail.com>
2025-01-24 12:22:07 -0300

    cicd: adding sonar

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-};
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov', 'text'], // Relatórios padrão: LCOV e texto
+  };
